22bb68d6c57e20c492ba22d41fba255aa1edf034
Wilson <wilsonreisccp@gmail.com>
2018-11-12 21:55:32 -0200

    P2: Aula 2 - Criando o menu da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component, ViewChild    } from '@angular/core';
+import { Platform, Nav     } from 'ionic-angular';
+import { StatusBar    } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
+import { HomePage     } from '../pages/home/home'
+import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos'
 
 @Component({
+  selector: 'myapp',
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = ListaAgendamentosPage.name;
+  @ViewChild(Nav) public nav: Nav;
+  
+  rootPage:any = HomePage;
+
+  public paginas = [
+    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' }
+  ];
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +25,11 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+  }
+  
+  irParaPagina(componente){
+    this.nav.push(componente);
   }
 }
 
--- src/pages/lista-agendamentos/lista-agendamentos.ts
+++ src/pages/lista-agendamentos/lista-agendamentos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Agendamento                 } from '../../modelos/agendamento';
 import { AgendamentoDaoProvider      } from '../../providers/agendamento-dao/agendamento-dao';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
