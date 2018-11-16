43a5af464037ab1613f6043b300b11fa381a9ac4
Wilson <wilsonreisccp@gmail.com>
2018-11-16 19:29:53 -0200

    P2: 4 - Criando a página de perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Platform, Nav         } from 'ionic-angular';
 import { StatusBar             } from '@ionic-native/status-bar';
 import { SplashScreen          } from '@ionic-native/splash-screen';
 import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos'
-import { LoginPage } from '../pages/login/login';
+import { LoginPage             } from '../pages/login/login';
+import { PerfilPage            } from '../pages/perfil/perfil';
+import { UsuariosServiceProvider } from '../providers/usuarios-service/usuarios-service';
 
 @Component({
   selector: 'myapp',
@@ -15,21 +17,26 @@ export class MyApp {
   rootPage:any = LoginPage;
 
   public paginas = [
-    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name, icone: 'calendar' }
+    { titulo: 'Perfil'      , componente: PerfilPage.name            , icone: 'person'   },
+    { titulo: 'Agendamentos', componente: ListaAgendamentosPage.name , icone: 'calendar' }
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen,
+              private _usuariosService: UsuariosServiceProvider) {
+                platform.ready().then(() => {
+                  statusBar.styleDefault();
+                  splashScreen.hide();
+                });
   }
   
   irParaPagina(componente){
     this.nav.push(componente);
+  }
+
+  get usuarioLogado(){
+    return this._usuariosService.obtemUsuarioLogado();
   }
 }
 
